6245d3eb3972fe43ddf75fffb85adcd864a380b5
Patrick-Cuppi <cuppi.patrick@gmail.com>
2025-04-08 12:37:46 -0300

    chore: create account repository db

--- internal/domain/account.go
+++ internal/domain/account.go
@@ -3,6 +3,7 @@ package domain
 import (
 	"crypto/rand"
 	"encoding/hex"
+	"sync"
 	"time"
 
 	"github.com/google/uuid"
@@ -14,6 +15,7 @@ type Account struct {
 	Email     string
 	APIKey    string
 	Balance   float64
+	mu        sync.RWMutex
 	CreatedAt time.Time
 	UpdatedAt time.Time
 }
@@ -38,4 +40,11 @@ func NewAccount(name, email string) *Account {
 	}
 
 	return account
+}
+
+func (a *Account) AddBalance(amount float64) {
+	a.mu.Lock()
+	defer a.mu.Unlock()
+	a.Balance += amount
+	a.UpdatedAt = time.Now()
 }
