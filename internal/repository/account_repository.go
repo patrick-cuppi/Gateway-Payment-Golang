9c087ee843ea5f5e194f27f47030f802d0f896bb
Patrick-Cuppi <cuppi.patrick@gmail.com>
2025-04-08 22:03:32 -0300

    feat: create func in account repository

--- internal/repository/account_repository.go
+++ internal/repository/account_repository.go
@@ -2,6 +2,7 @@ package repository
 
 import (
 	"database/sql"
+	"time"
 
 	"github.com/patrick-cuppi/Gateway-Payment-Golang/internal/domain"
 )
@@ -29,4 +30,92 @@ func (r *AccountRepository) Save(account *domain.Account) error {
 		return err
 	}
 	return nil
+}
+
+func (r *AccountRepository) FindByAPIKey(apiKey string) (*domain.Account, error) {
+	var account domain.Account
+	var createdAt, updatedAt time.Time
+
+	err := r.db.QueryRow(`
+		SELECT id, name, email, api_key, balance, created_at, updated_at
+		FROM accounts
+		WHERE api_key = $1
+	`, apiKey).Scan(
+		&account.ID,
+		&account.Name,
+		&account.Email,
+		&account.APIKey,
+		&account.Balance,
+		&createdAt,
+		&updatedAt,
+	)
+	if err == sql.ErrNoRows {
+		return nil, domain.ErrAccountNotFound
+	}
+	if err != nil {
+		return nil, err
+	}
+
+	account.CreatedAt = createdAt
+	account.UpdatedAt = updatedAt
+	return &account, nil
+}
+
+func (r *AccountRepository) FindByID(id string) (*domain.Account, error) {
+	var account domain.Account
+	var createdAt, updatedAt time.Time
+
+	err := r.db.QueryRow(`
+		SELECT id, name, email, api_key, balance, created_at, updated_at
+		FROM accounts
+		WHERE id = $1
+	`, id).Scan(
+		&account.ID,
+		&account.Name,
+		&account.Email,
+		&account.APIKey,
+		&account.Balance,
+		&createdAt,
+		&updatedAt,
+	)
+	if err == sql.ErrNoRows {
+		return nil, domain.ErrAccountNotFound
+	}
+	if err != nil {
+		return nil, err
+	}
+
+	account.CreatedAt = createdAt
+	account.UpdatedAt = updatedAt
+	return &account, nil
+}
+
+func (r *AccountRepository) UpdateBalance(account *domain.Account) error {
+	tx, err := r.db.Begin()
+	if err != nil {
+		return err
+	}
+	defer tx.Rollback()
+
+	// SELECT FOR UPDATE previne race conditions no saldo
+	var currentBalance float64
+	err = tx.QueryRow(`SELECT balance FROM accounts WHERE id = $1 FOR UPDATE`,
+		account.ID).Scan(&currentBalance)
+
+	if err == sql.ErrNoRows {
+		return domain.ErrAccountNotFound
+	}
+	if err != nil {
+		return err
+	}
+
+	_, err = tx.Exec(`
+        UPDATE accounts
+        SET balance = $1, updated_at = $2
+        WHERE id = $3
+    `, account.Balance, time.Now(), account.ID)
+	if err != nil {
+		return err
+	}
+	return tx.Commit()
 }
