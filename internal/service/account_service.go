4fb72419af73253fe677cb3fd5c56d554a699e83
Patrick-Cuppi <cuppi.patrick@gmail.com>
2025-04-09 12:52:45 -0300

    feat: create account func

--- internal/service/account_service.go
+++ internal/service/account_service.go
@@ -2,6 +2,7 @@ package service
 
 import (
 	"github.com/patrick-cuppi/Gateway-Payment-Golang/internal/domain"
+	"github.com/patrick-cuppi/Gateway-Payment-Golang/internal/dto"
 )
 
 type AccountService struct {
@@ -10,4 +11,24 @@ type AccountService struct {
 
 func NewAccountService(repository domain.AccountRepository) *AccountService {
 	return &AccountService{repository: repository}
+}
+
+func (s *AccountService) CreateAccount(input dto.CreateAccountInput) (*dto.AccountOutput, error) {
+	account := dto.ToAccount(input)
+
+	existingAccount, err := s.repository.FindByAPIKey(account.APIKey)
+	if err != nil && err != domain.ErrAccountNotFound {
+		return nil, err
+	}
+	if existingAccount != nil {
+		return nil, domain.ErrDuplicatedAPIKey
+	}
+
+	err = s.repository.Save(account)
+	if err != nil {
+		return nil, err
+	}
+
+	output := dto.FromAccount(account)
+	return &output, nil
 }
