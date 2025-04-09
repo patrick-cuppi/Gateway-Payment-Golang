15d5e6bf03dab06a6b147022409111c62a18fafb
Patrick-Cuppi <cuppi.patrick@gmail.com>
2025-04-09 12:59:52 -0300

    feat: create update balance func

--- internal/domain/repository.go
+++ internal/domain/repository.go
@@ -4,5 +4,5 @@ type AccountRepository interface {
 	Save(account *Account) error
 	FindByAPIKey(apiKey string) (*Account, error)
 	FindByID(id string) (*Account, error)
-	Update(account *Account) error
+	UpdateBalance(account *Account) error
 }
