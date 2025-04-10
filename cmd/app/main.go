af8f2f13ce4af1642f77ee3897c2d9d3a8d3d6f4
Patrick-Cuppi <cuppi.patrick@gmail.com>
2025-04-10 12:39:45 -0300

    chore: connection to database

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,3 +1,41 @@
 package main
 
-func main() {}
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+func getEnv(key, defaultValue string) string {
+	if value := os.Getenv(key); value != "" {
+		return value
+	}
+
+	return defaultValue
+}
+
+func main() {
+	if err := godotenv.Load(); err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	connStr := fmt.Sprintf(
+		"host=%s port=%s user=%s password=%s dbname=%s sslmode=%s",
+		getEnv("DB_HOST", "db"),
+		getEnv("DB_PORT", "5432"),
+		getEnv("DB_USER", "postgres"),
+		getEnv("DB_PASSWORD", "postgres"),
+		getEnv("DB_NAME", "gateway"),
+		getEnv("DB_SSL_MODE", "disable"),
+	)
+
+	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		log.Fatal("Error connecting to database", err)
+	}
+	defer db.Close()
+
+}
--- go.mod
+++ go.mod
@@ -5,4 +5,5 @@ go 1.24.1
 require (
 	github.com/go-chi/chi v1.5.5
 	github.com/google/uuid v1.6.0
+	github.com/joho/godotenv v1.5.1
 )
