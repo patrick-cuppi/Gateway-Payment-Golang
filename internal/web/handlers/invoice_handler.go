e480c19ae4b1060b6169c1e6e7393ba51a0cc3ee
Patrick-Cuppi <cuppi.patrick@gmail.com>
2025-04-11 19:16:10 -0300

    feat: created invoice handler func GetByID

--- internal/web/handlers/invoice_handler.go
+++ internal/web/handlers/invoice_handler.go
@@ -4,6 +4,8 @@ import (
 	"encoding/json"
 	"net/http"
 
+	"github.com/go-chi/chi"
+	"github.com/patrick-cuppi/Gateway-Payment-Golang/internal/domain"
 	"github.com/patrick-cuppi/Gateway-Payment-Golang/internal/dto"
 	"github.com/patrick-cuppi/Gateway-Payment-Golang/internal/service"
 )
@@ -36,5 +38,40 @@ func (h *InvoiceHandler) Create(w http.ResponseWriter, r *http.Request) {
 
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusCreated)
+	json.NewEncoder(w).Encode(output)
+}
+
+func (h *InvoiceHandler) GetByID(w http.ResponseWriter, r *http.Request) {
+	id := chi.URLParam(r, "id")
+	if id == "" {
+		http.Error(w, "ID is required", http.StatusBadRequest)
+		return
+	}
+
+	apiKey := r.Header.Get("X-API-KEY")
+	if apiKey == "" {
+		http.Error(w, "X-API-KEY is required", http.StatusBadRequest)
+		return
+	}
+
+	output, err := h.service.GetByID(id, apiKey)
+	if err != nil {
+		switch err {
+		case domain.ErrInvoiceNotFound:
+			http.Error(w, err.Error(), http.StatusNotFound)
+			return
+		case domain.ErrAccountNotFound:
+			http.Error(w, err.Error(), http.StatusUnauthorized)
+			return
+		case domain.ErrUnauthorizedAccess:
+			http.Error(w, err.Error(), http.StatusForbidden)
+			return
+		default:
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+	}
+
+	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(output)
 }
